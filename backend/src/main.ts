50997b8b030c8529a581d40a2d9e8372da82ab1c
shivamp-argus <shivamp@argusoft.com>
2023-05-23 12:06:52 +0530

    [added helmet middleware]

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
-
+import helmet from 'helmet'
 
 dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: "http://localhost:4200" })
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-  //   next();
-  // });
-  // app.enableCors({
-  //   origin: true,
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   credentials: true,
-  // });
+  app.use(helmet())
   await app.listen(3000);
 }
 bootstrap();
